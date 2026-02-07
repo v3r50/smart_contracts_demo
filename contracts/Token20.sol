e4325fadbf10c8df3c5702ec467d10292d803e02
v3r50 <lex.jankowska@gmail.com>
2026-02-07 23:02:03 +0100

    add owner-only mint

---
+++ contracts/Token20.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token20 is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("LexToken", "LEX") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+    _mint(to, amount);
+    }
+}
