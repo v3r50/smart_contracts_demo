5fcf779881a606fb912a58635fd954582ce4b088
v3r50 <lex.jankowska@gmail.com>
2026-02-08 02:25:19 +0100

    add (un)pause

--- contracts/Token721.sol
+++ contracts/Token721.sol
@@ -3,15 +3,16 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract Token721 is ERC721, Ownable {
+contract Token721 is ERC721, Ownable, Pausable {
     constructor() ERC721("LexNFT", "LEX") Ownable(msg.sender) {}
 
     event Minted(address indexed to, uint256 indexed tokenId);
 
     uint256 private _nextTokenId = 1;
 
-    function mint(address to) external onlyOwner {
+    function mint(address to) external onlyOwner whenNotPaused {
         uint256 tokenId = _nextTokenId;
         _nextTokenId++;
 
@@ -28,5 +29,13 @@ contract Token721 is ERC721, Ownable {
 
     function _baseURI() internal view override returns (string memory) {
         return _baseTokenURI;
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
