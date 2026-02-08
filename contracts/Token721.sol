82dd19c2e6936ccaf441b1e110ed7055c9f63fa1
v3r50 <lex.jankowska@gmail.com>
2026-02-08 02:06:58 +0100

    add mint

--- contracts/Token721.sol
+++ contracts/Token721.sol
@@ -1,1 +1,22 @@
-x
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token721 is ERC721, Ownable {
+    constructor() ERC721("LexNFT", "LEX") Ownable(msg.sender) {}
+
+    event Minted(address indexed to, uint256 indexed tokenId);
+    
+    uint256 private _nextTokenId = 1;
+
+    function mint(address to) external onlyOwner {
+        uint256 tokenId = _nextTokenId;
+        _nextTokenId++;
+
+        _safeMint(to, tokenId);
+
+        emit Minted(to, tokenId);
+    }
+}
